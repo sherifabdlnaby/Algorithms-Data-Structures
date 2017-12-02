e0af75290d0592f5506b1ee6afa377d03e7dcb97
sherifabdlnaby <sherifabdlnaby@gmail.com>
2017-12-02 03:27:11 +0200

    Small Clarity Change

--- [DS] BT - AVL - Balanced Binary Search Tree/main.cpp
+++ [DS] BT - AVL - Balanced Binary Search Tree/main.cpp
@@ -6,10 +6,11 @@ int main() {
     AVL tree;
     int x, n;
 
-    cout << "Enter the number of nodes" << endl;
+    cout << "Enter the number of Nodes" << endl;
     cin >> n;
 
     for (int i = 0; i < n; ++i) {
+        cout << "Enter Node No. " << i+1 << " ..." << endl;
         cin >> x;
         tree.insert(x);
     }
